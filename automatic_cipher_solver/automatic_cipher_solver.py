31cee547ae1b1783e32352303cec754f0cc30800
soapAndOoks <151615938+soapAndOoks@users.noreply.github.com>
2023-12-22 13:14:45 +0000

    Update automatic_cipher_solver.py

--- automatic_cipher_solver/automatic_cipher_solver.py
+++ automatic_cipher_solver/automatic_cipher_solver.py
@@ -1,4 +1,28 @@
-def return_best_key(self, iterations = 100, score_cutoff = -999999999999):
+from random import shuffle
+
+class CipherSolver():
+    def __init__(self, ciphertext, scoring_function, decrypt_function, parent_key1, parent_key2, child_key_generator):
+        self.ciphertext = ciphertext
+        self.scoring_function = scoring_function
+        self.decrypt_function = decrypt_function
+        self.parent_key1 = parent_key1
+        self.parent_key2 = parent_key2
+        self.child_key_generator = child_key_generator
+        self.__tmp_keys = []
+    
+    def __return_sorted_fittest_key(self, *args):
+        return sorted(args, key=lambda x: -self.scoring_function(self.decrypt_function(x, self.ciphertext)))
+
+    def __return_fittest_key(self, *args):
+        return self.__return_sorted_fittest_key(*args)[0]
+
+    def __check_keys_have_changed(self, *args):
+        if self.__tmp_keys != args:
+            self.__tmp_keys = args
+            return True
+        return False
+
+    def return_best_key(self, iterations = 100, score_cutoff = -999999999999):
         best_score = max(-999999999999, score_cutoff)
         best_key = ""
         for i in range(iterations):
