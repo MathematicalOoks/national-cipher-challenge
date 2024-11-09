69cb6ff3c5d49d5dd15c44abaa786ff519e07630
MathematicalOoks <88820859+MathematicalOoks@users.noreply.github.com>
2024-11-09 13:26:32 +0000

    Update caesar.py
    Removed unnecessary class

--- caesar.py
+++ caesar.py
@@ -1,27 +1,26 @@
-class Caesar:
-    def decrypt(self, text):
-        plaintext = ""
-        frequency_map = {}
-        
-        for i in text:
-            if 'A' <= i <= 'Z':
-                if i not in frequency_map:
-                    frequency_map[i] = 1
-                else:
-                    frequency_map[i] += 1
-
-        if frequency_map:
-            most_frequent_letter = max(frequency_map, key=frequency_map.get)
-        else:
-            return text
-
-        shift = ord(most_frequent_letter) - ord('E')
-
-        for i in text:
-            if 'A' <= i <= 'Z':
-                decrypted_char = chr((ord(i) - shift - 65) % 26 + 65)
-                plaintext += decrypted_char
+def decrypt(text):
+    plaintext = ""
+    frequency_map = {}
+    
+    for i in text:
+        if 'A' <= i <= 'Z':
+            if i not in frequency_map:
+                frequency_map[i] = 1
             else:
-                plaintext += i
+                frequency_map[i] += 1
 
-        return plaintext
+    if frequency_map:
+        most_frequent_letter = max(frequency_map, key=frequency_map.get)
+    else:
+        return text
+
+    shift = ord(most_frequent_letter) - ord('E')
+
+    for i in text:
+        if 'A' <= i <= 'Z':
+            decrypted_char = chr((ord(i) - shift - 65) % 26 + 65)
+            plaintext += decrypted_char
+        else:
+            plaintext += i
+
+    return plaintext
