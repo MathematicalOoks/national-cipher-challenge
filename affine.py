24f8297a8e10086be9a01b0befb35210b7710415
MathematicalOoks <88820859+MathematicalOoks@users.noreply.github.com>
2024-11-09 13:25:49 +0000

    Update affine.py
    Removed unnecessary class

--- affine.py
+++ affine.py
@@ -1,27 +1,26 @@
-class Affine:
-    def modInverse(self, a, m):
+def modInverse(a, m):
         for x in range(1, m):
             if (a * x) % m == 1:
                 return x
         return -1
 
-    def decrypt(self, text):
-        m = 26 
-        plaintext = ""
+def decrypt(text):
+    m = 26 
+    plaintext = ""
 
-        for a in range(1, 27):
-            for b in range(0, 26):
-                inverse = self.modInverse(a, m)
-                if inverse == -1:
-                    continue
-                
-                plaintext = ""
-                for i in text:
-                    if 'A' <= i <= 'Z':
-                        decrypted_char = (inverse * ((ord(i) - 65) - b)) % m
-                        plaintext += chr(decrypted_char + 65)
-                    else:
-                        plaintext += i
+    for a in range(1, 27):
+        for b in range(0, 26):
+            inverse = modInverse(a, m)
+            if inverse == -1:
+                continue
+            
+            plaintext = ""
+            for i in text:
+                if 'A' <= i <= 'Z':
+                    decrypted_char = (inverse * ((ord(i) - 65) - b)) % m
+                    plaintext += chr(decrypted_char + 65)
+                else:
+                    plaintext += i
 
-                if "@" not in plaintext:
-                    print(plaintext, '\n')
+            if "@" not in plaintext:
+                print(plaintext, '\n')
